c3a832a46fbd7b983e65798611ad79290430a5e9
ybsong <ybsong@goodcen.com>
2025-06-30 23:35:55 +0900

    //

--- rag/__init__.py
+++ rag/__init__.py
@@ -2,6 +2,7 @@ from rag.core.pdf_processor import PDFProcessor
 from rag.core.embedder import DocumentEmbedder
 from rag.core.chat_manager import ChatManager
 from rag.config.settings import Config
+from rag.handler.singal_handler import SignalHandler
 
 __version__ = "0.1.0"
-__all__ = ['PDFProcessor', 'DocumentEmbedder', 'ChatManager', 'Config']
+__all__ = ['PDFProcessor', 'DocumentEmbedder', 'ChatManager', 'Config', 'SignalHandler']
--- readme.md
+++ readme.md
@@ -1,2 +1,12 @@
 ### Python PDF 파일 Rag Test Program
 
+1. ollama install
+2. exaone3.5:latest install
+3. pip install
+4. main.py 실행
+5. files 위치에 있는 .pdf파일을 임베딩
+6. chatting
+```
+
+
+```
