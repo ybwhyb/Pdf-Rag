99756cd01cef410ca7e870ceaeea5fe2e26d7aef
ybsong <ybsong@goodcen.com>
2025-07-01 10:59:56 +0900

    //

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="docling",
+    name="PDF-Rag",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
@@ -12,8 +12,8 @@ setup(
         "langchain",
         "ollama",
     ],
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="ybsong",
+    author_email="ybwhyb@gmail.com",
     description="Document-based QA system using RAG",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
